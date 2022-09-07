a739edd0ecc031860ebf4a759960c3c1b0dcb680
bilalgoktas <bgoktas97@gmail.com>
2022-09-07 21:23:40 +0200

    created sidebar component without details, created icons.js for importing all icons in one file

--- src/components/sidebar/sidebar.jsx
+++ src/components/sidebar/sidebar.jsx
@@ -1,8 +1,46 @@
-import React from 'react'
+import React from 'react';
+import styles from "./sidebar.module.css";
 
 const Sidebar = () => {
   return (
-    <div>Sidebar</div>
+    <div>
+      <div className={styles.section}>
+        <button>Home</button>
+        <button>Home</button>
+        <button>Home</button>
+        <button>Home</button>
+      </div>
+      <div className={styles.section}>
+        <button>Home</button>
+        <button>Home</button>
+        <button>Home</button>
+        <button>Home</button>
+      </div>
+      <div className={styles.section}>
+        <button>Home</button>
+        <button>Home</button>
+        <button>Home</button>
+        <button>Home</button>
+      </div>
+      <div className={styles.section}>
+        <button>Home</button>
+        <button>Home</button>
+        <button>Home</button>
+        <button>Home</button>
+      </div>
+      <div className={styles.section}>
+        <button>Home</button>
+        <button>Home</button>
+        <button>Home</button>
+        <button>Home</button>
+      </div>
+      <div className={styles.section}>
+        <button>Home</button>
+        <button>Home</button>
+        <button>Home</button>
+        <button>Home</button>
+      </div>
+    </div>
   )
 }
 
