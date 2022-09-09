5d6b934dbbb10619ffbe7b077f83ce2a5c736849
bilalgoktas <bgoktas97@gmail.com>
2022-09-10 00:36:37 +0200

    added random video duration, fixed one digit integers to two digits

--- src/containers/home/home.jsx
+++ src/containers/home/home.jsx
@@ -49,7 +49,7 @@ const Home = () => {
         </div>
         <div className={styles.videoCards}>{
           
-          mapper.map((item, index) => <VideoCard key={index} title= {randomSentence({min:8, max:10}).slice(0, -1)} thumbnail={randomImage()} channelLogo={randomImage()} channel={randomSentence({min:1, max:3}).slice(0, -1)} views={`${randomNumber(10, 150)}${randomStringFromArray(characters)} views`} uploadedBefore={`${randomNumber(1, 7)} ${randomStringFromArray(time)} ago`}/>)
+          mapper.map((item, index) => <VideoCard key={index} title= {randomSentence({min:8, max:10}).slice(0, -1)} thumbnail={randomImage()} channelLogo={randomImage()} channel={randomSentence({min:1, max:3}).slice(0, -1)} views={`${randomNumber(10, 150)}${randomStringFromArray(characters)} views`} uploadedBefore={`${randomNumber(1, 7)} ${randomStringFromArray(time)} ago`} duration={`${randomNumber(0, 59).toLocaleString('en-US',{minimumIntegerDigits: 2})}:${randomNumber(0,59).toLocaleString('en-US',{minimumIntegerDigits: 2})}`}/>)
         }
         </div>
     </div>
